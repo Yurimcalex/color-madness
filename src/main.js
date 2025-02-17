283efd6cbebbd491a0516f1f2dfb5781ba5a4573
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-17 14:13:12 +0200

    Create main render fn

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-import './style.css'
+import './style.css';
+import generateColor from './generateColor.js';
+
+
+let counter = 1;
+
+function render() {
+  const colorElement = document.querySelector('.color');
+  colorElement.style.background = `${generateColor()}`;
+  colorElement.addEventListener('transitionend', (e) => {
+    if (counter > 10) return;
+    colorElement.style.background = `${generateColor()}`;
+    counter += 1;
+  });
+}
+
+
+render();
