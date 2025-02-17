bd7f1ac13651bcd2077148ea4ed8e7a69e33392b
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-17 18:31:28 +0200

    Create change number of colors feat

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './style.css';
 import generateColor from './generateColor.js';
 
-const colorElements = document.querySelectorAll('.color');
+let colorElements = document.querySelectorAll('.color');
 update(colorElements);
 
 
@@ -17,7 +17,35 @@ function setBackground(element) {
 }
 
 
+function remove(colorElements) {
+  for (let element of colorElements) {
+    element.remove();
+  }
+}
+
+
+function create(amount) {
+  const page = document.querySelector('.page');
+  while (amount--) {
+    const div = document.createElement('div');
+    div.classList.add('color');
+    page.prepend(div);
+  }
+  return document.querySelectorAll('.color');
+}
+
+
+function updateWidth(amount) {
+  let width = 100;
+  if (amount <= 4) width = 50;
+  else if (amount <= 16) width = 25;
+  else width = 12;
+  root.style.setProperty('--width', `${width}%`);
+}
+
+
 function render(colorElements) {
+  console.log('rendered!', colorElements);
   const callbacks = [];
 
   for (let element of colorElements) {
@@ -39,16 +67,32 @@ let reset;
 const startBtn = document.querySelector('.controls__start');
 const stopBtn = document.querySelector('.controls__stop');
 const speedRange = document.getElementById('transition_speed');
+const numberRange = document.getElementById('colors_number');
 const root = document.documentElement;
 
 startBtn.addEventListener('click', () => {
-  reset = render(colorElements);
+  if (reset) reset();
+  reset = render(document.querySelectorAll('.color'));
 });
 
 stopBtn.addEventListener('click', () => {
-  reset();
+  if (reset) reset();
 });
 
 speedRange.addEventListener("input", (e) => {
   root.style.setProperty('--timer', `${e.target.value}s`);
+});
+
+numberRange.addEventListener("change", (e) => {
+  const value = e.target.value;
+  document.getElementById('colors_number_result').textContent = value;
+  if (reset) reset();
+  remove(colorElements);
+  colorElements = create(+value);
+  updateWidth(+value);
+  update(colorElements);
+
+  setTimeout(() => {
+    reset = render(colorElements);
+  }, 0);
 });
