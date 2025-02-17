626556017062edffc4ed57065fec828ac0e77ed0
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-17 14:10:56 +0200

    Create color generator fn

---
+++ src/generateColor.js
@@ -0,0 +1,16 @@
+const HEXS = '0123456789abcdef';
+
+
+export default function generateColor() {
+  let color = '#';
+  while (color.length < 7) {
+    color += HEXS[ randomInt(0, 15) ];
+  }
+  return color;
+}
+
+
+function randomInt(min, max) {
+  let result = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(result);
+}
