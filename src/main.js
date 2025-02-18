6e773cf006cf5e652749c6c1d2eca5311314be46
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-18 19:56:13 +0200

    Set up the app

--- src/main.js
+++ src/main.js
@@ -1,122 +1,33 @@
 import './style.css';
-//import generateColor from './generateColor.js';
-
 import Colors from './Colors.js';
 import Controls from './Controls.js';
+import CSSData from './CSSData.js';
 
 
+const initalColorAmount = 1;
+const cssData = new CSSData();
 const colors = new Colors();
-const controls = new Controls();
+const controls = new Controls(cssData.transitionTime, initalColorAmount);
 
-
-colors.render(1);
-
+colors.render(initalColorAmount, cssData.transitionTime);
 
 controls.attachAction('start', 'click', () => {
-  colors.run();
+  colors.run(cssData.transitionTime);
 });
 
 controls.attachAction('stop', 'click', () => {
   colors.stop();
 });
 
+controls.attachAction('speed', 'input', (e) => {
+  const value = e.target.value;
+  cssData.setTransitionTime(+value);
+  document.getElementById('transition_speed_result').textContent = Number(value).toFixed(2) + 's';
+});
 
-
-// let colorElements = document.querySelectorAll('.color');
-// update(colorElements);
-
-
-// function update(colorElements) {
-//   for (let element of colorElements) {
-//     setBackground(element);
-//   }
-// }
-
-
-// function setBackground(element) {
-//   element.style.background = `${generateColor()}`;
-// }
-
-
-// function remove(colorElements) {
-//   for (let element of colorElements) {
-//     element.remove();
-//   }
-// }
-
-
-// function create(amount) {
-//   const page = document.querySelector('.page');
-//   while (amount--) {
-//     const div = document.createElement('div');
-//     div.classList.add('color');
-//     page.prepend(div);
-//   }
-//   return document.querySelectorAll('.color');
-// }
-
-
-// function updateWidth(amount) {
-//   let width = 100;
-//   if (amount <= 4) width = 50;
-//   else if (amount <= 16) width = 25;
-//   else width = 12;
-//   root.style.setProperty('--width', `${width}%`);
-// }
-
-
-// function render(colorElements) {
-//   console.log('rendered!', colorElements);
-//   const callbacks = [];
-
-//   for (let element of colorElements) {
-//     const callback = () => setBackground(element);
-//     element.addEventListener('transitionend', callback);
-//     callbacks.push(callback);
-//     callback();
-//   }
-  
-//   return () => {
-//     for (let element of colorElements) {
-//       element.removeEventListener('transitionend', callbacks.shift());
-//     }
-//   };
-// }
-
-
-// let reset;
-// const startBtn = document.querySelector('.controls__start');
-// const stopBtn = document.querySelector('.controls__stop');
-// const speedRange = document.getElementById('transition_speed');
-// speedRange.value = '1';
-// const numberRange = document.getElementById('colors_number');
-// numberRange.value = '1';
-// const root = document.documentElement;
-
-// startBtn.addEventListener('click', () => {
-//   if (reset) reset();
-//   reset = render(document.querySelectorAll('.color'));
-// });
-
-// stopBtn.addEventListener('click', () => {
-//   if (reset) reset();
-// });
-
-// speedRange.addEventListener("input", (e) => {
-//   root.style.setProperty('--timer', `${e.target.value}s`);
-//   document.getElementById('transition_speed_result').textContent = Number(e.target.value).toFixed(2) + 's';
-// });
-
-// numberRange.addEventListener("change", (e) => {
-//   const value = e.target.value;
-//   document.getElementById('colors_number_result').textContent = value;
-//   if (reset) reset();
-//   remove(colorElements);
-//   colorElements = create(+value);
-//   updateWidth(+value);
-//   update(colorElements);
-
-//   setTimeout(() => {
-//     reset = render(colorElements);
-//   }, 0);
-// });
+controls.attachAction('amount', 'change', (e) => {
+  const value = +e.target.value;
+  cssData.setColorWidth(value);
+  colors.render(value, cssData.transitionTime);
+  document.getElementById('colors_number_result').textContent = value;
+});
