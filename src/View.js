56fe81e231bd5e9a84601be1ca46cc2ddb62ab08
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-20 14:32:08 +0200

    Update View to create display pattern feat

--- src/View.js
+++ src/View.js
@@ -1,12 +1,29 @@
 export default class View {
 	constructor(speed, amount) {
+		this.container = document.querySelector('.page');
 		this.colorSpeed = document.getElementById('transition_speed_result');
 		this.colorAmount = document.getElementById('colors_number_result');
 		this.updateText('colorAmount', `${amount}`);
 		this.updateText('colorSpeed', `${speed}s`);
+		this.elements = [];
 	}
 
 	updateText(elementName, newText) {
 		this[elementName].textContent = newText;
+	}
+
+	resetPattern() {
+		this.elements.forEach(element => element.element.remove());
+	}
+
+	displayPattern(patternName, elements) {
+		const nodes = elements.map(element => element.element);
+		this.resetPattern();
+		this[patternName](nodes);
+		this.elements = elements;
+	}
+
+	linear(nodes) {
+		nodes.forEach(node => this.container.prepend(node));
 	}
 }
