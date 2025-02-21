d15326d42c6dfc254bdcaae8447bda3093ddc079
Yuri Sklyarov <yuri.mcalex@gmail.com>
2025-02-21 09:49:06 +0200

    Update main module to use the settings

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './style.css';
 import Colors from './Colors.js';
 import Controls from './Controls.js';
 import View from './View.js';
-import { randomInt } from './generateColor.js';
+import Settings from './Settings.js';
 
 
 const initalColorAmount = 7;
@@ -11,24 +11,7 @@ const initialColorSpeed = 1;
 const colors = new Colors(document.querySelector('.page'));
 const controls = new Controls(initialColorSpeed, initalColorAmount);
 const view = new View(initialColorSpeed, initalColorAmount);
-
-
-const settings = {
-  speed: initialColorSpeed,
-  amount: initalColorAmount,
-  speeds: mapTransitionTime(initalColorAmount, initialColorSpeed),
-  pattern: 'linear'
-};
-
-
-function mapTransitionTime(amount, speed) {
-  const speeds = [];
-  while (amount--) {
-    if (speed) speeds.push(speed);
-    else speeds.push(randomInt(1, 30) / 10);
-  }
-  return speeds;
-}
+const settings = new Settings(initialColorSpeed, initalColorAmount);
 
 
 colors.render(settings.speeds);
@@ -47,9 +30,7 @@ controls.attachAction('stop', 'click', () => {
 
 controls.attachAction('speed', 'input', (e) => {
   const newSpeed = +e.target.value;
-  settings.speed = newSpeed;
-  settings.speeds = mapTransitionTime(settings.amount, newSpeed);
-
+  settings.setValue('speed', newSpeed);
   colors.render(settings.speeds);
   view.displayPattern(settings.pattern, colors.elements);
   view.updateText('colorSpeed', `${newSpeed.toFixed(2)}s`);
@@ -58,15 +39,7 @@ controls.attachAction('speed', 'input', (e) => {
 
 controls.attachAction('amount', 'change', (e) => {
   const colorAmount = +e.target.value;
-  const isRandomSpeed = controls.randomSpeed.checked;
-  settings.amount = colorAmount;
-
-  if (isRandomSpeed) {
-    settings.speeds = mapTransitionTime(settings.amount);
-  } else {
-    settings.speeds = mapTransitionTime(settings.amount, settings.speed);
-  }
-
+  settings.setValue('amount', colorAmount);
   colors.render(settings.speeds);
   view.displayPattern(settings.pattern, colors.elements);
   view.updateText('colorAmount', colorAmount);
@@ -80,13 +53,11 @@ controls.attachAction('open', 'click', () => {
 
 controls.attachAction('randomSpeed', 'change', (e) => {
   if (e.target.checked) {
-    settings.speeds = mapTransitionTime(settings.amount);
     controls.speed.disabled = true;
   } else {
-    settings.speeds = mapTransitionTime(settings.amount, settings.speed);
     controls.speed.disabled = false;
   }
-
+  settings.setValue('isRandomSpeed', e.target.checked);
   colors.render(settings.speeds);
   view.displayPattern(settings.pattern, colors.elements);
 });
@@ -94,8 +65,7 @@ controls.attachAction('randomSpeed', 'change', (e) => {
 
 controls.attachAction('pattern', 'change', (e) => {
   const pattern = e.target.value;
-  settings.pattern = pattern;
-
+  settings.setValue('pattern', pattern);
   colors.render(settings.speeds);
   view.displayPattern(settings.pattern, colors.elements);
 });
